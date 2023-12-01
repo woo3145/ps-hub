c3b89c4395496d581bab944b969582c1f94342a8
이창우 <lcwoo3145@gmail.com>
2023-12-01 23:40:11 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+SELECT o.ANIMAL_ID, o.NAME 
+    from ANIMAL_INS i
+    inner join ANIMAL_OUTS o
+    on i.ANIMAL_ID = o.ANIMAL_ID
+    where o.DATETIME < i.DATETIME
+    order by i.DATETIME
