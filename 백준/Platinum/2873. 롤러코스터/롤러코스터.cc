df089c5d7820d44d81cc237454e489fa27e10a5e
이창우 <lcwoo3145@gmail.com>
2024-01-01 20:07:31 +0900

    [Platinum III] Title: 롤러코스터, Time: 80 ms, Memory: 8980 KB -BaekjoonHub

--- 백준/Platinum/2873. 롤러코스터/롤러코스터.cc
+++ 백준/Platinum/2873. 롤러코스터/롤러코스터.cc
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
 #include <string>
 
 using std::ios;
@@ -8,50 +7,13 @@ using std::cout;
 using std::cin;
 using std::vector;
 using std::string;
-using std::pair;
-
-// 3 3
-// 5 1 3
-// 2 4 8
-// 1 1 2
 
 int N, M;
 vector<vector<int>> board;
 
-//
-// 짝짝
-// 1 4 11 ?        
-// 8 6 ?  1
-
-
-
-// 1 4 1 5        
-// 4 6 3  6
-// 4 4 11 4     
-// 2 6 2  1
-
-// 홀수가 섞여있으면 짝수쪽으로 먼저 이동 시 모든칸 방문 가능
-// 짝홀
-// 1 4 11 6 7
-// 8 5 4  6 1       
-// 1 4 11 6 7
-// 8 5 4  6 1
-
-// 홀짝
-// 1 4 11 20        
-// 8 6 3  1
-// 1 4 11 1
-
-// 어느쪽으로 가든 모든칸 방문 가능
-// 홀홀
-// 1 4 11       
-// 8 6 3  
-// 1 4 1
-
 int maxCost = 0;
 int dy[4] = {-1, 0, 1, 0};
 int dx[4] = {0, 1, 0, -1};
-char ds[4] = {'U', 'R', 'D', 'L'};
 
 void Input() {
     cin >> N >> M;
@@ -97,7 +59,7 @@ void Solve() {
             }
             if(i != M - 1) answer += "R";
         }
-    // 짝짝 dfsxxxxxxxxx
+    // 짝짝
     }else{
         int tC = 1000000;
         int tY, tX = 0;
@@ -116,15 +78,8 @@ void Solve() {
         }
 
         int line = tY - (tY & 1);
-        // 1 x 5 x 2 x     
-        // x 6 x 1 x 5
-        // 1 x 4 x 6 x     (3, 2)  2
-        // x 6 x 1 x 2     D R U R (R) D R U R D
-        // 8 x 5 x 5 x     (2, 1)  2
-        // x 6 x 1 x 2     D R (R) U R D R U R D
 
         for(int i = 0; i < N; ++i){
-            // 타겟이 존재하는 2줄 처리
             if(i < line){
                 for(int j = 0; j < M - 1; ++j){
                     if(i % 2 == 0){
@@ -135,7 +90,6 @@ void Solve() {
                 }
                 answer += "D";
             }else if(i == line){
-                bool flag = true;
                 for(int j = 0; j < M - 1; ++j){
                     if(j < tX){
                         if(j % 2 == 0){
@@ -163,7 +117,6 @@ void Solve() {
                 }
             }
         }
-        
     }
 
     cout << answer;
@@ -179,10 +132,3 @@ int main() {
     
     return 0;
 }
-
-
-// 4 4
-// 1 1 4 5        
-// 4 6 3  6
-// 4 4 11 4     
-// 2 6 2  1
