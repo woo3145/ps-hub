ee9cfc2bd000d5653931b9036e60fe6f167525b5
이창우 <lcwoo3145@gmail.com>
2024-01-17 15:44:54 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 284 ms, Memory: 5932 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using std::ios;
+using std::cout;
+using std::cin;
+using std::vector;
+
+int N;
+vector<int> arr;
+
+void Solve() {
+    std::sort(arr.rbegin(), arr.rend());
+
+    for(auto& a : arr){
+        cout << a << '\n';
+    }
+}
+
+void Input() {
+    cin >> N;
+    arr.resize(N);
+
+    for(auto& n : arr){
+        cin >> n;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    Input();
+    Solve();    
+    
+    return 0;
+}
